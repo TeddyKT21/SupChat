5cdb1ba35795e891aa4d6fa4a95ee078388a1d31
Tomer <tomerrim@gmail.com>
2023-05-15 10:37:39 +0300

    changed line to layoutline

--- sup-chat-client/src/pages/chats.jsx
+++ sup-chat-client/src/pages/chats.jsx
@@ -1,18 +1,18 @@
 import {MainLayout} from "../UIkit/Layouts/MainLayout/MainLayout";
 import { SideBar } from "../UIkit/Components/SideBar/SideBar";
-import { Line } from "../UIkit/Layouts/Line/Line";
+import { LayoutLine } from "../UIkit/Layouts/Line/Line";
 import { ChatArea } from "../UIkit/Components/ChatArea/ChatArea";
 import { ChatCard } from "../UIkit/Components/Cards/ChatCard/ChatCard";
 import "./PageStyles/chat.css";
 
 export const Chats = () => {
 
-    return ( 
-        <div className="chats">
-            <Line>
-                <SideBar />
-                <ChatArea/>
-            </Line>
-        </div>
-    )
+    return (
+      <div className="chats">
+        <LayoutLine>
+          <SideBar />
+          <ChatArea />
+        </LayoutLine>
+      </div>
+    );
 }

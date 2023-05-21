b7f35e4d9cbc7704b5be04529602c6b68ee99b91
teddy K <teddy.klay@gmail.com>
2023-05-21 16:48:19 +0300

    messages and other info run on socketio

--- sup-chat-client/src/UIkit/Components/ChatArea/ChatArea.jsx
+++ sup-chat-client/src/UIkit/Components/ChatArea/ChatArea.jsx
@@ -6,8 +6,7 @@ import { Input } from "../Input/Input/Input";
 import { Saparate } from "../../Layouts/Line/Line";
 import { Button } from "../Button/Button";
 import { sendMessage } from "../../../store/userSlice";
-import { UseFetch } from "../../../CustomHooks/useFetch";
-import { connectSocket,disconnectSocket, emitMessage } from '../../../services/socket';
+import { connectSocket,disconnectSocket, emitMessage, listenToMessages } from '../../../services/socket';
 import "./ChatArea.css";
 
 export const ChatArea = () => {
@@ -26,7 +25,7 @@ export const ChatArea = () => {
 
     useEffect(() => {
         if(user){
-            connectSocket(user.username);
+            connectSocket(user);
         }
 
         return () => {
--- sup-chat-server/dist/services/io.js
+++ sup-chat-server/dist/services/io.js
@@ -2,21 +2,19 @@ import { Server } from "socket.io";
 import chatEvents from "./chatio.js";
 import messageEvents from "./messageio.js";
 import userEvents from "./userio.js";
-function addEvents(socket, functions, names) {
+function addEvents(socket, io, functions, names) {
     console.log('adding events...');
     for (let i = 0; i < names.length; i++) {
-        socket.on(names[i], (data) => functions[i](data, socket));
+        socket.on(names[i], async (data) => await functions[i](data, io, socket));
     }
 }
 export function initSocketIO(server) {
     const io = new Server(server, { cors: { origin: "*" } });
     // const io : socketIO.Server = socketIO(server)
     io.on("connection", (socket) => {
-        addEvents(socket, chatEvents.functions, chatEvents.eventNames);
-        addEvents(socket, messageEvents.functions, messageEvents.eventNames);
-        addEvents(socket, userEvents.functions, userEvents.eventNames);
-        socket.on('test', (test) => console.log('test !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'));
-        console.log("Client connected:", socket.id);
+        addEvents(socket, io, chatEvents.functions, chatEvents.eventNames);
+        addEvents(socket, io, messageEvents.functions, messageEvents.eventNames);
+        addEvents(socket, io, userEvents.functions, userEvents.eventNames);
     });
 }
 //# sourceMappingURL=io.js.map

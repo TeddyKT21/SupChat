451d8272a6e763d4645f7040acea86fb4cd8fc2e
Tomer <tomerrim@gmail.com>
2023-06-05 21:28:45 +0300

    added back handleDrawerOpen and handleDrawerClose

--- sup-chat-client/src/UIkit/Components/ChatInfo/ChatInfo.jsx
+++ sup-chat-client/src/UIkit/Components/ChatInfo/ChatInfo.jsx
@@ -32,6 +32,14 @@ export const ChatInfo = ({chat}) => {
   const fileInput = useRef(null);
   const isAdmin = chat?.admins?.includes(user_id);
 
+  const handleDrawerOpen = () => {
+    setDrawerOpen(true);
+  };
+
+  const handleDrawerClose = () => {
+    setDrawerOpen(false);
+  };
+
   const handleFileInput = () => {
     if(fileInput.current){
       fileInput.current.click();
